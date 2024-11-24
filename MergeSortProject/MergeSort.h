a1e315886844437156999e34bc99db42ee45459a
Oleh <oleh456yt@gmail.com>
2024-11-25 00:04:38 +0100

    Merge Sort

---
+++ MergeSortProject/MergeSort.h
@@ -0,0 +1,13 @@
+﻿#pragma once
+#include <vector>
+#include <stdexcept>
+
+// Klasa implementująca algorytm sortowania przez scalanie
+class MergeSort {
+public:
+    static void sort(std::vector<int>& arr);
+
+private:
+    static void mergeSort(std::vector<int>& arr, int left, int right);
+    static void merge(std::vector<int>& arr, int left, int middle, int right);
+};
